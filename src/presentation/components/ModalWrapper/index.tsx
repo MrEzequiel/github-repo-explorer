150fda778b22cb96304534100e33c7ceddb76bb5
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2024-05-30 16:51:36 -0500

    feat: creating modal wrapper

---
+++ src/presentation/components/ModalWrapper/index.tsx
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import { createPortal } from "react-dom";
+
+import useDisableScroll from "../../../hooks/useDisableScroll";
+
+interface IModalWrapperProps {
+  children: React.ReactNode
+  onClose: () => void
+}
+
+export function ModalWrapper({ children, onClose }: IModalWrapperProps) {
+  useDisableScroll()
+  const contentRef = useRef<HTMLDivElement>(null)
+
+  const handleClickBackdrop = (event: React.MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLDivElement
+
+    if (contentRef.current && !contentRef.current.contains(target)) {
+      onClose()
+    }
+  }
+
+  return createPortal(
+    <div
+      className="opacity-0 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none animate-fade bg-black/70 p-4"
+      onClick={handleClickBackdrop}
+    >
+      <div className="relative w-full p-9 max-w-5xl my-6 mx-auto bg-gray-950 shadow-lg" ref={contentRef}>
+        {children}
+      </div>
+    </div>,
+    document.body
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ export default {
     fontFamily: {
       sans: ['"Raleway", sans-serif'],
     },
-    extend: {},
+    extend: {
+      animation: {
+        fade: "fadeIn 200ms ease-in-out forwards",
+      },
+
+      keyframes: () => ({
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      }),
+    },
   },
   plugins: [],
 };
